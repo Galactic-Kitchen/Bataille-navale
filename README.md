aaf7839ddcfd627b8d04c4283e8c08c771aa69ba
Galactic-Kitchen <136994993+Galactic-Kitchen@users.noreply.github.com>
2023-10-02 23:38:48 +0200

    ca avance toujours
    ca marche pas x)

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Bataille navale
  Bataille Navale en C
  
-Voir pour jouer contre ordi et taille tableau jeu variable 
+Voir pour jouer contre ordi et taille tableau jeu variable éventuellement
 Finir fonction affichage
 améliorer transmission arguments en tableau aux affichages
 optimiser
+double tableau valeurs pour connaitre joueur 1 et joueur 2
+règles de vérification à faire plus tard
 
+règles :
+10*10 cases
+```1*5 + 1*4 + 2*3 +1*2
--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main (void) {
     short taille_jeu=3; /* ca sera utilisé plus tard */
 	char statut;
 	int compteur_partie=0, compteur_victoire=0; /*compteur_partie - compteur_victoire = compteur joueur 2*/
+	nouvelle_partie_question(&statut); /*lancement programme*/
 	for (;statut!='q';) {
 		if (statut=='a') {
 			affichage_scores(compteur_partie, compteur_victoire);
@@ -19,45 +20,47 @@ int main (void) {
 		nouvelle_partie_question(&statut);
 		}
 	}
-
     return EXIT_SUCCESS;
 }
 
 
-int nouvelle_partie_question (sortie) {
+int nouvelle_partie_question (char *sortie) {
 	char reponse;
-	printf("nouvelle partie ?\na : afficher les scores, y : lancer une nouvelle partie, q : quitter")
+	printf("nouvelle partie ?\na : afficher les scores, y : lancer une nouvelle partie, q : quitter");
 	reponse=getchar();
-	while (reponse!='y' or reponse!='a' or reponse !='q') {
+	while (reponse!='y' || reponse!='a' || reponse !='q') { /*bug quelque part / reformatage à faire*/
 		printf("réessayer\n");
 		reponse=getchar();
 	}
-	&sortie=reponse;
+	*sortie=reponse;
 	return EXIT_SUCCESS;
 }
 
 
 int affichage_scores (c_p, c_v) {
-	printf(" nombre de parties jouées, nombre de partie gagnées par joueur 1, nombre de parties gagnées par joueur 2\n %5d%5c %5d%5c%5d", c_p, SEPARATEUR, c_v, SEPARATEUR, c_p-c_v);
+	printf("nombre de parties jouées, nombre de partie gagnées par joueur 1, nombre de parties gagnées par joueur 2\n %5d %5c %5d %5c %5d", c_p, SEPARATEUR, c_v, SEPARATEUR, c_p-c_v);
 	return EXIT_SUCCESS;
 }
 
 int jeu (void) {
+	short Taille_jeu=3;
 	int victoire, statut=1;
 	char position[9];
 	while (statut==1) {
 		
 	
 	
-	
+	}
 	return victoire;
+	}
 
 
 
 int affichage (tableau) {
+	short Taille_jeu=3;
     int i,k,t;
     for (i=0;i<Taille_jeu;i++) { //première ligne affichage tableau
-        printf("\9c", 'a'+i);
+        printf("%9c", 'a'+i);
     }
     for (k=0;k<Taille_jeu;k++) { //boucle séparation et cases
         for (t=0;t<Taille_jeu;t++) { //boucle séparateur
